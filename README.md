50db04b26521374b89f6c8292f1a0e16f7d28d8b
Michael Paukov <michael.paukov@gmail.com>
2022-01-10 12:55:13 +0100

    add project files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # goit-js-hw-11
+
 Home Work 11
+
+Модуль 11. CRUD операции и синтаксис async/await
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const DEBOUNCE_DELAY = 300;
